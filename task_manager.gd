e1b63b4ecf12c5b0f21220dcd47b102acc015ea5
Brodzman <35158237+Brodzman@users.noreply.github.com>
2025-02-20 11:12:19 +1100

    Add spam calling task

--- task_manager.gd
+++ task_manager.gd
@@ -6,17 +6,37 @@ extends Node
 
 @onready var task_label: Label = $"../PlaceholderHUD/ColorRect/Task"
 
-signal task(task)
+var text_track
+
+signal task(task, description)
+
+func _ready() -> void:
+	$"../Greybox/Phone".connect("spam_call_done", Callable(self, "_on_spam_call_done"))
+	task_label.text = "Tasks"
+	text_track = task_label.text
 
 func _on_timer_timeout() -> void:
-	var task = randi_range(1, 1)
+	var description
+	var task = randi_range(1, 2)
+	
 	if task == 1: # Friend calls
 		task = "friend_call"
-		task_label.text = "Task: Friend is calling"
-		emit_signal("task", task)
+		description = " | Friend is calling"
+		task_label.text = text_track + description
+		text_track = task_label.text
+		emit_signal("task", task, description)
 		
 	elif task == 2: # Spam calls
-		pass
+		var i = 0
+		while i < 5:
+			task = "spam_call"
+			description = " | Spam call"
+			task_label.text = text_track + description
+			text_track = task_label.text
+			emit_signal("task", task, description)
+			await $"../Greybox/Phone".spam_call_done
+			await get_tree().create_timer(3).timeout
+			i += 1
 		
 	elif task == 3: # Water plant
 		pass
@@ -29,3 +49,6 @@ func _on_timer_timeout() -> void:
 		
 	elif task == 6: # Make your own food
 		pass
+		
+func _on_spam_call_done(new_text):
+	text_track = new_text
