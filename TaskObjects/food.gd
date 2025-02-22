19fcf44d0ec0caa5c6221dd93f254f1eefacb89a
Brodzman <35158237+Brodzman@users.noreply.github.com>
2025-02-23 00:25:10 +1100

    Allow the food to be dropped

--- TaskObjects/food.gd
+++ TaskObjects/food.gd
@@ -1,9 +1,26 @@
 extends Interactable
 
-signal food_in_hand
 var in_hand = false
+var food_tracker = 0
+
+signal food_in_hand
+
+@onready var player: CharacterBody3D = $"../../Player"
+@onready var interact_ray: RayCast3D = $"../../Player/Head/Camera3D/InteractRay"
 
 func _on_interacted(body: Variant) -> void:
 	visible = false
+	set_collision_layer_value(2, false)
 	in_hand = true
+	self.set_name(str(food_tracker))
 	emit_signal("food_in_hand")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("drop_food"):
+		in_hand = false
+		visible = true
+		set_collision_layer_value(2, true)
+		if self.name == str(food_tracker):
+			var drop_offset = Vector3(0, 0, 2)
+			global_position = interact_ray.global_position - interact_ray.global_transform.basis.z
+			food_tracker += 1
