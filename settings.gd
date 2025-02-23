befdee41df03430bebf31e930d22ebf9300b2214
gourdfarm-games <gourdfarm.fbx@gmail.com>
2025-02-22 19:58:24 -0500

    New assets & pause menu
    Edited UI
    Changed Q to Q&B
    Added menu splash art
    Added painting
    Added table
    Added pause menu
    Added UI scaling

--- settings.gd
+++ settings.gd
@@ -16,3 +16,15 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+
+func _on_resolutions_item_selected(index: int) -> void:
+	match index:
+		0: 
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1: 
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2: 
+			DisplayServer.window_set_size(Vector2i(1280,720))
+		3: 
+			DisplayServer.window_set_size(Vector2i(2560,1440))
