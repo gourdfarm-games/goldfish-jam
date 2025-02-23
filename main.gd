a9dbdc020d3e9888c5c51b679e04c8cb32b171b3
gourdfarm-games <gourdfarm.fbx@gmail.com>
2025-02-23 13:19:22 -0500

    Added in game quick restart

--- main.gd
+++ main.gd
@@ -1,11 +1,24 @@
 extends Node3D
 
 @onready var pause_menu = $pausemenu
+@onready var restart_menu = $restartmenu
 var paused = false
+var restartmenu = false 
 
 func _process(delta):
 	if Input.is_action_just_pressed("return-to-menu"):
 		toggle_pause()
+	if Input.is_action_just_pressed("restart"):
+		toggle_restartmenu()
+
+
+func toggle_restartmenu():
+	restartmenu = !restartmenu
+	
+	if restartmenu:
+		get_tree().change_scene_to_file("res://main.tscn")
+	else:
+		restart_menu.hide()
 
 func toggle_pause():
 	paused = !paused  
