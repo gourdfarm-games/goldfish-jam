a0bc475bdb7b30cdbdb252f9ee643aa2574634da
Brodzman <35158237+Brodzman@users.noreply.github.com>
2025-03-02 16:00:44 +1100

    Organize Scenes

---
+++ Scripts/restart_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_restart_pressed() -> void:
+	$button.play()
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	Engine.time_scale = 1.0
+
+func _on_menu_pressed() -> void:
+	$button.play()
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	Engine.time_scale = 1.0
