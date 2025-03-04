2f5a00e7291c617700ec1c5fa4d36fda2f9cc57e
Brodzman <35158237+Brodzman@users.noreply.github.com>
2025-03-04 16:40:15 +1100

    Fix audio sliders

--- Scripts/Menus/menu.gd
+++ Scripts/Menus/menu.gd
@@ -3,12 +3,11 @@ extends Control
 func _on_play_pressed() -> void:
 	$button.play()
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
-	
 
 func _on_settings_pressed() -> void:
 	$button.play()
 	get_tree().change_scene_to_file("res://Scenes/Menus/settings.tscn")
-
+	
 func _on_credits_pressed() -> void:
 	$button.play()
 	get_tree().change_scene_to_file("res://Scenes/Menus/credits.tscn")
